aed4c8b6d36d9c769100cb430e494f6a46301d23
Chamz <chamz_210@hotmail.com>
2023-01-18 10:09:55 -0500

    Create express app and connect mongoDB

---
+++ node-rest-api/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app= express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,()=>{
+
+    console.log("connected to MONGODB")
+});
+
+app.listen(8800,() =>{
+    console.log("Backend server is running!")
+})
