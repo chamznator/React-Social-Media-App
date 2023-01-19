dc8fdab9b012cfe0c7037302045f89ec2d6745bf
Chamz <chamz_210@hotmail.com>
2023-01-19 12:36:12 -0500

    Create mongoDB scehem for users

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -11,6 +11,13 @@ mongoose.connect(process.env.MONGO_URL,()=>{
 
     console.log("connected to MONGODB")
 });
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800,() =>{
     console.log("Backend server is running!")
