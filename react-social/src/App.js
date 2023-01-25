b1cc211f0448e4830cf9badce823cd1582f0eede
Chamz <chamz_210@hotmail.com>
2023-01-25 15:36:48 -0500

    create login  page

--- react-social/src/App.js
+++ react-social/src/App.js
@@ -1,10 +1,12 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 
 
  
 
 function App() {
-  return <Home/>
+  return <Login/>
 }
 
 export default App;
