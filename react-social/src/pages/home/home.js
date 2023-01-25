67f204077e06bc7daaeebc4ad9da56bf09f7e28f
Chamz <chamz_210@hotmail.com>
2023-01-24 19:34:19 -0500

    add sidebar component and style page

--- react-social/src/pages/home/home.js
+++ react-social/src/pages/home/home.js
@@ -1,5 +1,17 @@
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css"
 
-export default function home() {
-  return <Topbar/>
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      
+        <Sidebar />
+        
+      
+    </>
+  );
 }
