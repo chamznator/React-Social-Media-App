649eaa967e61e34662371686956171508d460418
Chamz <chamz_210@hotmail.com>
2023-01-26 11:21:23 -0500

    fix post get router using params instead of body

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const app= express();
+const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL,()=>{
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
 
-    console.log("connected to MONGODB")
-});
+//middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
@@ -19,6 +27,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800,() =>{
-    console.log("Backend server is running!")
-})
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
